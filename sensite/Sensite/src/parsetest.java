774641d76f056a4a2f9614ef27d5e049b01f5d34
Rose Liu <roseliu2011@gmail.com>
2014-02-14 14:52:07 -0800

    Web site front end

---
+++ sensite/Sensite/src/parsetest.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author roseyliu
+ */
+import com.parse.*;
+public class parsetest {
+
+
+public static void onCreate() {
+Parse.initialize(this, "Pi3RwsAGmn3ySRY4O7nothMRE7fKCAdF0jfVHrkk", "neoPlCVOCPSFH1oz8PJttL7mE4omfe4GyWFHdpC6");
+}
+    
+}
+
+public void main(){
+parsetest.onCreate();
+}
