b1e58c460541e88b5e013f964af45fa228ebc77c
ntgsx92 <Sixiang@SixiangMac-2.local>
2014-03-10 16:29:05 -0700

    use long-term access token to work around request token issue

--- sensite/Sensite/src/java/SensiteFacebook.java
+++ sensite/Sensite/src/java/SensiteFacebook.java
@@ -5,10 +5,6 @@
 import java.util.Map;
 import java.util.HashMap;
 import java.io.File;
-import java.io.BufferedWriter;
-import java.io.BufferedReader;
-import java.io.FileWriter;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.FileInputStream;
 import java.io.ObjectInputStream;
@@ -30,7 +26,7 @@ import org.json.JSONException;
 public class SensiteFacebook {
     
         /*
-        */    
+        
         private static AccessToken GenerateAccessToken(){
             String app_id = "237493219771359";
             String app_secret = "6c15d69456051330d3ab5a943fb1d02d";
@@ -42,6 +38,7 @@ public class SensiteFacebook {
             } 
             return accessToken;
         }
+        */
         
         /*
         WriteLastTimeStamp will write time stamp of the
@@ -124,8 +121,9 @@ public class SensiteFacebook {
              //Load previous time stamp
              String prev_time_stamp = ReadLastestTimeStamp();
              //Generate Access Token
-             AccessToken accessToken = GenerateAccessToken();
-             FacebookClient facebookClient = new DefaultFacebookClient(accessToken.getAccessToken());
+             //AccessToken accessToken = GenerateAccessToken();
+             String long_term_access_token = "CAADXZC7MAQ98BAFyCiE9yE1GeFZCZB96eDQ2fGSivS3PMZCEeIrS88DOZC0CdllpynJAO2yf0aGJuxiFNnIUbq2O7v1iuNSxRI8zedxmGDNkl2GQROh5T3J1jgIegeFBwNcgBeN9mT45LhKrpZCKDcYQV8S4xzchXdHEEmtpkJl8SsMmMqhfOZC";
+             FacebookClient facebookClient = new DefaultFacebookClient(long_term_access_token);
              //Load new posts from Facebook Wall
              Map<String, String> posts_map = GetNewPosts(prev_time_stamp, facebookClient);
              for(Map.Entry<String, String> entry : posts_map.entrySet()){
